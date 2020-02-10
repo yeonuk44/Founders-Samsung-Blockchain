4f93c149b06c749c371881ec03e4f4f5d240177c
YeonUk Kim <40634025+yeonuk44@users.noreply.github.com>
2020-02-10 15:42:13 +0900

    first upload
    Samsung SDK Platform

---
+++ main/java/com/example/myfirstas/MainActivity.java
@@ -0,0 +1,174 @@
+package com.example.myfirstas;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.ledger.lib.apps.eth.Eth;
+import com.samsung.android.sdk.blockchain.CoinType;
+import com.samsung.android.sdk.blockchain.ListenableFutureTask;
+import com.samsung.android.sdk.blockchain.SBlockchain;
+import com.samsung.android.sdk.blockchain.account.Account;
+import com.samsung.android.sdk.blockchain.coinservice.CoinNetworkInfo;
+import com.samsung.android.sdk.blockchain.exception.AccountException;
+import com.samsung.android.sdk.blockchain.exception.RemoteClientException;
+import com.samsung.android.sdk.blockchain.exception.RootSeedChangedException;
+import com.samsung.android.sdk.blockchain.exception.SsdkUnsupportedException;
+import com.samsung.android.sdk.blockchain.network.EthereumNetworkType;
+import com.samsung.android.sdk.blockchain.wallet.HardwareWallet;
+import com.samsung.android.sdk.blockchain.wallet.HardwareWalletType;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button connectBtn;
+    Button generateAccountBtn;
+    Button getAccountsBtn;
+    Button paymentBtn;
+    Button sendsmartcontractapiBtn;
+    SBlockchain sBlockchain = new SBlockchain();
+    private HardwareWallet wallet;
+    private Account mAccount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        sBlockchain = new SBlockchain();
+        try {
+            sBlockchain.initialize( this);
+
+        } catch (SsdkUnsupportedException e ) {
+            e.printStackTrace();
+        }
+
+        connectBtn = findViewById(R.id.connect);
+        generateAccountBtn = findViewById(R.id.generateAccount);
+        getAccountsBtn = findViewById(R.id.getAccount);
+        paymentBtn = findViewById(R.id.payment);
+        sendsmartcontractapiBtn = findViewById(R.id.sendsmartcontractapi);
+
+        connectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connect();
+            }
+        });
+
+        generateAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generate();
+
+            }
+        });
+
+        getAccountsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getAccounts();
+            }
+        });
+    }
+
+    private void getAccounts(){
+        List<Account> accounts = sBlockchain.getAccountManager()
+                .getAccounts(wallet.getWalletId(), CoinType.ETH, EthereumNetworkType.ROPSTEN);
+        Log.d("Myapp", Arrays.toString(new List[]{accounts}));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void connect(){
+        sBlockchain.getHardwareWalletManager()
+                .connect(HardwareWalletType.SAMSUNG,true)
+                .setCallback(new ListenableFutureTask.Callback<HardwareWallet>() {
+                    @Override
+                    public void onSuccess(HardwareWallet hardwareWallet) {
+                        wallet = hardwareWallet;
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull ExecutionException e) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NotNull InterruptedException e) {
+
+                    }
+                });
+    }
+
+    private void generate(){
+        CoinNetworkInfo coinNetworkInfo = new CoinNetworkInfo(
+                CoinType.ETH,
+                EthereumNetworkType.ROPSTEN,
+                "https://ropsten.infura.io/v3/c390e494bf26472ab6cf48f14be05495"
+        );
+
+        sBlockchain.getAccountManager()
+                .generateNewAccount(wallet,coinNetworkInfo)
+                .setCallback(new ListenableFutureTask.Callback<Account>() {
+                    @Override
+                    public void onSuccess(Account account) {
+                        mAccount = account;
+                        Log.d("MyApp",account.toString());
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull ExecutionException e) {
+                        Log.d("MyApp",e.getMessage());
+                    }
+
+                    @Override
+                    public void onCancelled(@NotNull InterruptedException e) {
+
+                    }
+                });
+
+        }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
