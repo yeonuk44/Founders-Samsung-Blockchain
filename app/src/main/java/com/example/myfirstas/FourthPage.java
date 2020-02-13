c59072a225fcdbb9959ffa4ad35bae3627e3845a
YeonUk Kim <40634025+yeonuk44@users.noreply.github.com>
2020-02-13 16:45:32 +0900

    create android intent for java files
    create android intent (Foundation completion for .java file) 
    implement screen to screen (final)

--- app/src/main/java/com/example/myfirstas/FourthPage.java
+++ app/src/main/java/com/example/myfirstas/FourthPage.java
@@ -2,13 +2,28 @@ package com.example.myfirstas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FourthPage extends AppCompatActivity {
+
+    Button fourthtosecondBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth_page);
+
+        fourthtosecondBtn = findViewById(R.id.fourthtosecond);
+
+        fourthtosecondBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(),SecondPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
